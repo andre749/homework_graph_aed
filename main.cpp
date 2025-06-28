06312a4781eac1b3420106021b97ec76ea99de67
andre <andrecvalera06@gmail.com>
2025-06-28 18:55:03 -0500

    logre que la wea compile RAAAAAAAAAAAAAAAH

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include "gui.h"
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
+
 int main() {
-    cout<<"hola";
+    // Crear una ventana de 800x600 píxeles, con título "Ventana de prueba"
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Ventana de prueba");
+
+    // Bucle principal
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            // Cerrar la ventana si se presiona la X
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Limpiar la ventana (color negro por defecto)
+        window.clear();
+
+        // Mostrar lo que se ha dibujado
+        window.display();
+    }
+
+    return 0;
 }
