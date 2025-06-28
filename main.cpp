b54353a9585e6dc4b74e1941b9f3063af49c9aa8
andre <andrecvalera06@gmail.com>
2025-06-28 18:57:56 -0500

    no c

--- main.cpp
+++ main.cpp
@@ -1,24 +1,7 @@
-#include <SFML/Graphics.hpp>
+#include "gui.h"
 
 int main() {
-    // Crear una ventana de 800x600 píxeles, con título "Ventana de prueba"
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Ventana de prueba");
-
-    // Bucle principal
-    while (window.isOpen()) {
-        sf::Event event{};
-        while (window.pollEvent(event)) {
-            // Cerrar la ventana si se presiona la X
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // Limpiar la ventana (color negro por defecto)
-        window.clear();
-
-        // Mostrar lo que se ha dibujado
-        window.display();
-    }
-
+    GUI gui("nodes.csv", "edges.csv");
+    gui.main_loop();
     return 0;
 }
